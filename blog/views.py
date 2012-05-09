830059a7acc237f7a96a2c7446f26e76af888746
waylybaye <waylybaye@wayly.net>
2012-05-09 22:11:19 +0800

    autocomplate in 中文

--- blog/views.py
+++ blog/views.py
@@ -43,6 +43,6 @@ def search(request):
     q = request.GET.get('q')
     q = ' '.join([t.decode('utf8') for t in seg_txt(q.encode('utf8')) ])
     queryset = SearchQuerySet()
-    #results = queryset.autocomplete(content_auto=q)
-    results = queryset.filter(text=q)
+    results = queryset.autocomplete(content_auto=q) | queryset.filter(text=q)
+    #results = queryset.filter(text=q)
     return render_to_response('blog/search.html', {'results': results, 'q': q}, RequestContext(request))
