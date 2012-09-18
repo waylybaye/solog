391e9d5370892b8c88feced230339fa44bd35758
waylybaye <havelove@gmail.com>
2012-09-18 20:48:59 +0800

    update blog style

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ def index(request):
     """
     Index page
     """
-    posts = Post.objects.order_by('-id')[:2]
+    posts = Post.objects.order_by('-id')[:10]
     return render_to_response("index.html", {'posts': posts}, RequestContext(request))
 
 
