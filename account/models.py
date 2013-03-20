58eaae7de253aa63b9557a697bfa08ffeb4c0daa
waylybaye <havelove@gmail.com>
2013-03-20 23:34:35 +0800

    add DropboxToken model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
+from datetime import datetime
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class DropboxToken(models.Model):
+    user = models.ForeignKey(User, related_name="dropbox_tokens")
+    access_token = models.CharField(max_length=100)
+    access_token_secret = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(default=datetime.now)
+
+    def __unicode__(self):
+        return self.access_token
