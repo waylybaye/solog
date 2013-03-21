16593191449a21fa81a718a341c6ceb955855d11
waylybaye <havelove@gmail.com>
2013-03-22 00:04:10 +0800

    add DropboxToken and Blog to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Blog
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("user", "title", "is_published", "last_update_at")
+
+admin.site.register(Blog)
+admin.site.register(Post, PostAdmin)
