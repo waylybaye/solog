17220a2fa4f115229d289c22eeff23d221a7b2e2
waylybaye <havelove@gmail.com>
2013-05-04 21:15:39 +0800

    remove account app

--- account/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from account.models import DropboxToken
-
-
-class DropboxTokenAdmin(admin.ModelAdmin):
-    list_display = ("user", "access_token", "access_token_secret", "created_at")
-
-admin.site.register(DropboxToken, DropboxTokenAdmin)
--- account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('account.views',
-    url(r'^login$', 'account_login', name='login'),
-    url(r'^logout$', 'account_logout', name='logout'),
-    url(r'^dropbox/auth$', 'dropbox_auth', name='dropbox_auth'),
-    url(r'^dropbox/callback$', 'dropbox_auth', {'callback': True}, name='dropbox_callback'),
-    url(r'^dropbox/sync$', 'dropbox_sync', name='dropbox_sync'),
-)
